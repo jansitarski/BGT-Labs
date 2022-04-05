9e222b28e2453ebad0cc51c424b796986f51b9b6
Jan Sitarski <jansitarski00@gmail.com>
2022-04-05 17:43:53 +0200

    lab4

--- Lab04/setupClient.sh
+++ Lab04/setupClient.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get -y install python3 python3-pip git
-sudo pip3 install dask distributed --upgrade
+sudo apt-get -y install python3 pip git
+sudo pip install dask[complete]
 sudo pip3 install pyarrow
+export PATH="/home/s20701/.local/bin:$PATH"
 git clone https://github.com/jansitarski/BGT-Labs
 cd BGT-Labs/Lab04/
 
--- Lab04/setupScheduler.sh
+++ Lab04/setupScheduler.sh
@@ -4,4 +4,3 @@ sudo apt-get update
 sudo apt-get -y install python3 python3-pip
 sudo pip3 install dask distributed --upgrade
 dask-scheduler --port 8000
-
