7482ba47ca6b1d926797821bc6050b3efe69b536
Jan Sitarski <jansitarski00@gmail.com>
2022-04-07 14:12:10 +0200

    bucket

--- Lab04/main2.py
+++ Lab04/main2.py
@@ -6,7 +6,7 @@ from dask_cloudprovider.gcp import GCPCluster
 # gcsfs
 if __name__ == "__main__":
     ##cluster = GCPCluster(on_host_maintenance="STOP", projectid="bgt-labs", n_workers=3, machine_type="e2-small", source_image="https://www.googleapis.com/compute/v1/projects/debian-cloud/global/images/debian-11-bullseye-v20220406")
-    with GCPCluster(on_host_maintenance="STOP", env_vars=dict(EXTRA_PIP_PACKAGES="pyarrow"), projectid="bgt-labs",
+    with GCPCluster(on_host_maintenance="STOP", env_vars=dict(EXTRA_PIP_PACKAGES="pyarrow"), projectid="bgt-labs-s20701",
                     n_workers=3, machine_type="e2-small") as cluster:
         with Client(cluster) as client:
             df = dd.read_parquet(
