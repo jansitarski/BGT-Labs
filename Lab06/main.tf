39221ef476a65e70638fa38b6c05728832e75940
jansitarski00@gmail.com <jansitarski00@gmail.com>
2022-04-17 13:48:37 +0200

    all VMs

--- Lab06/main.tf
+++ Lab06/main.tf
@@ -32,3 +32,63 @@ resource "google_compute_instance" "firstmachine" {
   metadata_startup_script = file("./SetupAndRun.sh")
 
 }
+resource "google_compute_instance" "secondmachine" {
+  name         = "secondmachine"
+  machine_type = "n2-standard-4"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      size  = 50
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+  metadata_startup_script = file("./SetupAndRun.sh")
+
+}
+
+resource "google_compute_instance" "thirdmachine" {
+  name         = "thirdmachine"
+  machine_type = "n2-standard-2"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      size  = 100
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+  metadata_startup_script = file("./SetupAndRun.sh")
+
+}
+
+resource "google_compute_instance" "fourthmachine" {
+  name         = "fourthmachine"
+  machine_type = "n2-standard-4"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      size  = 100
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+  metadata_startup_script = file("./SetupAndRun.sh")
+
+}
+
